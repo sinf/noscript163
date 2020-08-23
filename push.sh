5be7511f3732b6ddb22c067f2c7fdcb8780f9360
ArhoM <arho.iivari@elisanet.fi>
2020-08-23 16:08:52 +0300

    push.sh, rsync.lst

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#scp art.py amahl.fi:~/news.amahl.fi/art.py
+#scp zh-news/zh-articles.css amahl.fi:~/news.amahl.fi/zh-news/zh-articles.css
+rsync -azivcR --skip-compress=jpg/webp/png/gif/mp4/gz/bz2 -e ssh --rsync-path=~/bin/rsync $(cat rsync.lst) amahl.fi:~/news.amahl.fi/
+
